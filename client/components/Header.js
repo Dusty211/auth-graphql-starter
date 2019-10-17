8741080e3159a0866f4e514fddb919a794ad39cc
Kyle Houghton <kylebenz@hotmail.com>
2019-10-17 16:00:29 -0400

    fixed issues and continuing on to vid98

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
+import { Link } from 'react-router';
 import query from '../queries/CurrentUser';
+import mutation from '../mutations/Logout';
 
 class Header extends Component {
+    onLogoutClick(){
+        this.props.mutate({
+            refetchQueries: [{ query }]
+        });
+    }
 
     renderButtons(){
         const { loading, user } = this.props.data;
@@ -10,10 +17,19 @@ class Header extends Component {
         if (loading) {return <div />; }
 
         if (user) {
-            return <div>Logout</div>;
+            return (
+                <li><a onClick={this.onLogoutClick.bind(this)}>Logout</a></li>
+            );
         } else {
             return (
-                <div>You're not signed in.</div>
+                <div>
+                    <li>
+                        <Link to="/signup">Signup</Link>
+                    </li>
+                    <li>
+                        <Link to="/login">Login</Link>
+                    </li>
+                </div>
             );
         }
 
@@ -22,11 +38,18 @@ class Header extends Component {
         return(
             <div>
                 <nav className="nav-wrapper">
-                    {this.renderButtons()}
+                    <Link to="/" className="brand-logo">
+                        Home
+                    </Link>
+                    <ul className="right">
+                        {this.renderButtons()}
+                    </ul>
                 </nav>
             </div>
         )
     }
 }
 
-export default graphql(query)(Header);
+export default graphql(mutation)(
+    graphql(query)(Header)
+);
